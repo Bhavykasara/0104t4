2225bd0ee4dc991e5e9c0f9090cd8df74fe3894d
Bhavykasara <78550596+Bhavykasara@users.noreply.github.com>
2021-06-16 18:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+Webcam.attach("#camera");
+
+
+
+Webcam.snap({
+    function(data_url){
+        document.getElementById("camera").innerHTML='<img src="'+data_url+'">';
+    }
+});
+
+/*--vedio=document.getElementById("camera");
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(stream=> {
+        vedio.srcObject=stream;
+    }).catch(function(err){
+        console.log("Something went wrong");
+        console.log(err);
+    });
+
+function selfie() {
+    vedio2=document.getElementById("result");
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(stream2=> {
+        vedio2.srcObject=stream2;
+    });
+}
+
+console.log("ml5 version",ml5.version);--*/
